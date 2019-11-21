e020eb75516d17954f79e0ea52ccb4e7a3fc34af
Cristi B <cbetivu@gmail.com>
2019-11-21 22:56:05 +0000

    Clean-up in files/notebooks; group the shares by hour

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import configs
 from merge_csv import merge_files
 from cycle_data_cleaning import clean_data
@@ -7,9 +6,8 @@ from weather_codes import save_weather_codes
 from weather_download import init_weather_download
 from cycling_data_downloader import download_cycle_data
 
-def main():
+def main(key):
     configs.configure_logging()
-    key = '8189b301e612478682b103404191011'
 
     download_cycle_data(configs.cycling_data_index, configs.cycling_data_dl_dir)
     clean_data(configs.cycling_data_dl_dir, configs.restructured_data_dir)
@@ -18,3 +16,7 @@ def main():
     init_weather_download(key, configs.weather_data_dl_dir, configs.weather_data_city)
     save_weather_codes(configs.weather_data_dl_dir, configs.weather_data_codes)
     write_weather_data(configs.weather_data_dl_dir, configs.weather_data_csv, configs.EOL)
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1])
